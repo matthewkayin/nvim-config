969eb8edd5219df1b7e368913673ea61c745d892
Matthew Madden <matthewkayin@gmail.com>
2025-07-29 14:41:30 -0500

    add indent line

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -8,7 +8,7 @@ return {
         },
         config = function()
             local cmp = require('cmp')
-            cmp.setup {
+            cmp.setup({
                 auto_brackets = {},
                 completion = { completeopt = "menu,menuone,noinsert" },
                 mapping = cmp.mapping.preset.insert {
@@ -18,8 +18,16 @@ return {
                     { name = 'nvim_lsp' },
                     { name = 'path' },
                     { name = 'buffer', option = { keyword_length = 3 }}
-                }
-            }
+                },
+                enabled = function()
+                    local disabled = false
+                    disabled = disabled or (vim.api.nvim_get_option_value('buftype', { buf = 0 }) == 'prompt')
+                    disabled = disabled or (vim.fn.reg_recording() ~= ' ')
+                    disabled = disabled or (vim.fn.reg_executing() ~= ' ')
+                    disabled = disabled or require('cmp.config.context').in_treesitter_capture('comment')
+                    return not disabled
+                end,
+            })
         end
     }
 }
